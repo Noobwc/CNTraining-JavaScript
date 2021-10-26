63934608f7e774a29daf73af1ca05f0c5b271075
wangchao <chao.wang2@thoughtworks.com>
2021-10-26 20:19:49 +0800

    [wangchao] feat: complete js chapter-one homework

--- chapter-one/homework/practise.js
+++ chapter-one/homework/practise.js
@@ -2,29 +2,51 @@
 const removeRepeat = () => {
     const array = [1, 2, 2, 3, 3, 3, 'a', 'a', 'b'];
     // todo
+    const result = [...new Set(array)];
     // 输出[1, 2, 3, "a", "b"]
+    console.log(result);
 }
 removeRepeat();
 
 //问题2
-const findRepeat= () => {
+const findRepeat = () => {
     const array = [1, 2, 2, 3, 3, 3, 'a', 'a', 'b'];
     // todo
+    var newArr = [];
+    for (var j = 0; j < array.length; j++) {
+        for (var i = j + 1; i < array.length; i++) {
+            if (array[j] == array[i]) {
+                newArr.push(array[i]);
+            }
+        }
+    }
     // 输出[2, 3, "a"]
+    console.log([...new Set(newArr)])
 }
 findRepeat();
 
 
 //问题3
-const clone= () => {
+const clone = (obj) => {
     //todo
+    if (typeof obj === 'function') {
+        throw new TypeError('请传入正确的数据类型格式')
+    }
+    try {
+        let josnData = JSON.stringify(obj)
+        let newObj = JSON.parse(josnData)
+        return newObj
+    } catch (e) {
+        console.log(e)
+    }
 }
-const obj = {
+let obj = {
     arr: [{a: 1, f: {q: 1}}, 2, 3],
     obj: {key: 'value'},
     string: 'hello world',
 };
 const obj1 = clone(obj);
+obj['string'] = 'nihao'
 console.log(obj1.string === obj.string);
 
 
@@ -37,6 +59,19 @@ const question4= () => {
     }
     console.log(i)
     // todo
+    for (var i = 0; i < 5; i++) {
+        (function(j) {
+            setTimeout(function () {
+                console.log(j);
+            }, 1000);
+        })(i);
+    }
     //修改代码输出 5,0,1,2,3,4
+    console.log(i)
 }
 question4();
+
+//问题5 声明会被提升（函数提升在变量之前）
+// 所以先输出了function a() {}
+// 然后就是输出两次666
+// 最后输出了function() {};
